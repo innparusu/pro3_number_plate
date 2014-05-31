ce57f748fef3689d4e1cf4642d5c7f0f1402c35c
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-06-01 03:28:49 +0900

    Show images
    ```
    $ vim app.rb
    $ vim views/index.erb
    $ vim views/show.erb
    ```

--- app.rb
+++ app.rb
@@ -12,20 +12,22 @@ class Image < ActiveRecord::Base
 end
 
 get '/' do
-    "Hello #{Image.count} images!"
+  "Hello #{Image.count} images!"
+  @images = Image.all
+  erb :index
 end
 
 post '/create' do
   image      = Image.new
   image.data = params[:upload_file]
-  image.save
-  puts params[:image]
-  File.open("./image/#{@image.id}.jpg", "wb") do |file|
-    file.write(@image.data)
+  if image.save
+    File.open("./public/images/#{image.id}.jpg", "wb") do |file|
+      file.write(image.data)
+    end
   end
 end
 
 get '/:id/show' do
-  @image_path = "./image/#{params[:id]}.jpg"
+  @image_path = "./images/#{params[:id]}.jpg"
   erb :show
 end
