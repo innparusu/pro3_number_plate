c335c10c0bc7b60f681550395757d1840c732a4c
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2014-06-01 01:43:11 +0900

    Add action
    ```
    $ vim app.rb
    $ git rm --cached number_palate.sqlite3
    ```

--- app.rb
+++ app.rb
@@ -1,7 +1,31 @@
 require 'sinatra'
 require 'active_record'
 
+set :bind, '0.0.0.0'
+
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
   database: 'number_palate.sqlite3'
 )
+
+class Image < ActiveRecord::Base
+end
+
+get '/' do
+    "Hello #{Image.count} images!"
+end
+
+post '/create' do
+  image      = Image.new
+  image.data = params[:upload_file]
+  image.save
+  redirect "#{image.id}/show"
+end
+
+get '/:id/show' do 
+  @image = Image.find(params[:id])
+  File.open("./#{@image.id}.jpg", "wb") do |file|
+    file.write(@image.data)
+  end
+  @image.data
+end
